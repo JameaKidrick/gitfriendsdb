375973c0d2b2f89827aad1b64f091f52966dccdf
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-11-30 00:48:27 -0500

    pulled from master

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server');
 
-const PORT = process.env.PORT || 5556;
+const PORT = process.env.PORT || 5555;
 
 server.listen(PORT, () => {
   console.log(`\n=== LISTENING TO PORT ${PORT} ===\n`)
